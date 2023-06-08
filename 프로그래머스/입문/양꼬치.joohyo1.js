e52cfcb216f9b590b1ea16a953bdedc59f2e4c8b
joohyo1 <juhyowork@gmail.com>
2023-06-08 16:13:17 +0900

    프로그래머스: 양꼬치

--- 프로그래머스/입문/양꼬치.joohyo1.js
+++ 프로그래머스/입문/양꼬치.joohyo1.js
@@ -1,17 +1,14 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+function solution(n, k) {
+  let lamb = n * 12000;
+  let drink = k * 2000;
+  let answer = lamb + drink;
 
-int solution(int n, int k) {
-    int answer = 0;
-    int lamb = n*12000;
-    int drink = k*2000;
-    
-    if(n/10>0){
-        answer = (sheep+drink)-n/10*2000;
-    } else {
-        answer = sheep+drink;
-    }
-    
-    return answer;
+  if (n > 10) {
+    let service = parseInt(n / 10);
+    return (answer = answer - service * 2000);
+  } else if (n === 10) {
+    answer = answer - 2000;
+  }
+
+  return answer;
 }
